fbe83c17257966a7e8adf2ada640c4a08916f6c1
Anthony <anthonychinaemeru84@yahoo.com>
2024-07-01 23:39:16 +0100

    Allowed Host changed

--- server/settings.py
+++ server/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0ns&kh7az#tik)km%xgs(51t23hge^vh!n6r3n23(*pys%ipwr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['tydev01.pythonanywhere.com ']
 
 
 # Application definition
