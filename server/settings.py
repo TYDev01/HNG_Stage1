52e5bbaafe9f2df409e660d5d37995dcdf6398c3
Anthony <anthonychinaemeru84@yahoo.com>
2024-07-02 00:34:16 +0100

    Change the Allowed Host

--- server/settings.py
+++ server/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0ns&kh7az#tik)km%xgs(51t23hge^vh!n6r3n23(*pys%ipwr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['server.vercel.app']
 
 
 # Application definition
