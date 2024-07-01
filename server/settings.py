ee10a295eee4cdd859d5b5a02789742eb5784f2f
Anthony <anthonychinaemeru84@yahoo.com>
2024-07-02 00:38:14 +0100

    Allowed host modified

--- server/settings.py
+++ server/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0ns&kh7az#tik)km%xgs(51t23hge^vh!n6r3n23(*pys%ipwr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['server.vercel.app']
+ALLOWED_HOSTS = ['server.vercel.app', 'https://hng-stage1-ruddy.vercel.app/']
 
 
 # Application definition
