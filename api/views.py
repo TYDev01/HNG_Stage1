8ac4b6aa5f607ad47f90d2b7d0d075375aed64e2
Anthony <anthonychinaemeru84@yahoo.com>
2024-07-02 12:33:07 +0100

    Tried a new ip

--- api/views.py
+++ api/views.py
@@ -17,22 +17,19 @@ def newVisitor(request):
 
     try:
         get_client_ip = requests.get("https://api.ipgeolocation.io/getip")
-        get_client_ip.raise_for_status()
         client_ip = get_client_ip.json().get("ip")
     except requests.RequestException:
         client_ip = "127.0.0.1" #Falls back default ip
 
     try:
         ipgeo_response = requests.get(f"https://api.ipgeolocation.io/ipgeo?apiKey={API_KEY}&ip={client_ip}")
-        ipgeo_response.raise_for_status()
-        client_city = ipgeo_response.json().get('city', 'Unknown')
+        client_city = ipgeo_response.json().get('city',)
     except requests.RequestException:
         client_city = "Unknown"
 
     try:
         weather_response = requests.get(f'http://api.openweathermap.org/data/2.5/weather?q={client_city}&appid={WEATHER_API}&units=metric')
-        weather_response.raise_for_status()
-        temperature = weather_response.json().get('main', {}).get('temp', 'Unknown')
+        temperature = weather_response.json().get('main', {}).get('temp',)
     except requests.RequestException:
         temperature = "Unknown"
 
