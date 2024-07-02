beae976504a99cafdf18c9711023a2e4043f527b
Anthony <anthonychinaemeru84@yahoo.com>
2024-07-02 15:51:02 +0100

    Switch from ipgeolocation.io to ipinfo.io

--- api/views.py
+++ api/views.py
@@ -16,20 +16,22 @@ def newVisitor(request):
 
 
     try:
-        get_client_ip = requests.get("https://api.ipgeolocation.io/getip")
+        # get_client_ip = requests.get("https://api.ipgeolocation.io/getip")
+        # client_ip = get_client_ip.json().get("ip")
+        get_client_ip = requests.get('https://ipinfo.io')
         client_ip = get_client_ip.json().get("ip")
     except requests.RequestException:
         client_ip = "127.0.0.1" #Falls back default ip
 
     try:
         ipgeo_response = requests.get(f"https://api.ipgeolocation.io/ipgeo?apiKey={API_KEY}&ip={client_ip}")
-        client_city = ipgeo_response.json().get('city',)
+        client_city = ipgeo_response.json().get('city')
     except requests.RequestException:
         client_city = "Unknown"
 
     try:
         weather_response = requests.get(f'http://api.openweathermap.org/data/2.5/weather?q={client_city}&appid={WEATHER_API}&units=metric')
-        temperature = weather_response.json().get('main', {}).get('temp',)
+        temperature = weather_response.json().get('main', {}).get('temp')
     except requests.RequestException:
         temperature = "Unknown"
 
